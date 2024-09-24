974912dc3fb08625e2185ea256b63e66c5772249
Gorewhun <gor22010@byui.edu>
2024-09-24 11:20:44 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Part 1 and 2, user chooses the number
+
+        // Console.Write("What is the magic number? ");
+        // int magic_number = int.Parse(Console.ReadLine());
+
+        // Part 3, Random number generator
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,10);
+
+
+        int guess = -1;
+
+        // While loop to keep user guessing the magic number
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+       
+
+
     }
 }
