300039b9a10d461479515433ba815d49ac4d67d2
Gorewhun <gor22010@byui.edu>
2024-09-24 11:54:40 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,5 +1,6 @@
 using System;
  using System.Collections.Generic;
+using System.Runtime.InteropServices;
 class Program
 {
     static void Main(string[] args)
@@ -17,7 +18,37 @@ class Program
         string user_response = Console.ReadLine();
         user_number = int.Parse(user_response);
 
-
+        if (user_number != 0)
+        {
+            numbers.Add(user_number);
+        }
        }
+
+        // Part 1 compute the sum
+        int sum = 0;
+        
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}.");
+
+        // Part 2 Average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}.");
+
+        // Part 3 Max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}.");
     }
 }
+
+
