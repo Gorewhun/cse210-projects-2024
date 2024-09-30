eb8050e9201259b053adbf7e7c70b04248aa96fd
Gorewhun <gor22010@byui.edu>
2024-09-30 13:01:41 -0600

    Added Program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello My name is Hunter!");
+        List<double> x = new();
+        // x = [24.0, 25.0, 26.0];
+
+        Console.WriteLine("Hello Sandbox World!");  
+
+        // Gather data to fill up a list
+
+        double input = -1;
+        while(input != 0)
+        {
+            Console.Write("Enter something: ");
+            string input_string = Console.ReadLine();
+            input = double.Parse(input_string);
+            x.Add(input);
+        }
+       
+        foreach (double number in x)
+        {
+            Console.WriteLine(number);
+        }    
     }
+
 }
---
+++ sandbox/Sandbox2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Sandbox2;
+
+class Program
+{
+    static void AwsomeFunction(int x)
+    {
+        Console.WriteLine($"the argument recieved by the awesome function is {x}.");
+    }
+    static void Main(string[] args)
+    
+    {
+        AwsomeFunction(188999);
+    }
+}
